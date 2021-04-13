2cb0e3b82a77789bed4d02147ad4975198d0da7a
mqz05 <60781507+mqz05@users.noreply.github.com>
2021-04-13 16:43:47 +0200

    Add files via upload

---
+++ Virus Blaster/Virus Blaster/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Virus Blaster
+//
+//  Created by tallerapps on 13/04/2021.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
